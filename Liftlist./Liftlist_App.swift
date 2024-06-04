aca4fb4907268ecddfba1e9f486ee1e734b976c3
henryle <le000379@umn.edu>
2024-06-03 19:44:03 -0500

    Initial Commit

---
+++ Liftlist./Liftlist_App.swift
@@ -0,0 +1,17 @@
+//
+//  Liftlist_App.swift
+//  Liftlist.
+//
+//  Created by Henry Le on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Liftlist_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
